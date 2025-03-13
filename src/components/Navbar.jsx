79c3be1fef6c656d75895a3087964a60e655718f
Secuzi <secre2k@gmail.com>
2025-03-13 09:14:55 +0800

    Finished navbar layout

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+export default function Navbar() {
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="w-full h-16 md:h-20 flex items-center justify-between">
+      {/* Logo */}
+      <div className="flex items-center gap-4 text-2xl font-bold">
+        <img src="/logo.png" alt="Company Logo" className="w-8 h-8" />
+        <span>HeyBlogger</span>
+      </div>
+
+      {/* Mobile Menu */}
+      <div className="md:hidden">
+        <div
+          className="cursor-pointer text-4xl"
+          onClick={() => setOpen((prevState) => !prevState)}
+        >
+          {open ? "X" : "☰"}
+        </div>
+        {/* Mobile Link list */}
+        <div
+          className={`w-full h-screen flex flex-col items-center justify-center absolute gap-8 font-medium text-lg top-16 bg-[#e6e6ff] transition-all ease-in-out ${
+            open ? "-right-0" : "-right-[100%]"
+          }`}
+        >
+          <a href="/">Home</a>
+          <a href="/">Trending</a>
+          <a href="/">Most Popular</a>
+          <a href="/">About</a>
+          <a href="">
+            <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
+              Log in 👋
+            </button>
+          </a>
+        </div>
+      </div>
+      {/* Desktop Menu */}
+      <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+        <a href="/">Home</a>
+        <a href="/">Trending</a>
+        <a href="/">Most Popular</a>
+        <a href="/">About</a>
+        <a href="">
+          <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
+            Log in 👋
+          </button>
+        </a>
+      </div>
+    </div>
+  );
+}
