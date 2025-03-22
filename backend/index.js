0daf364a0e6e66e2483a6788730918020459e6e0
Secuzi <secre2k@gmail.com>
2025-03-22 12:51:01 +0800

    Created models for user, commend, and post

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 import express from "express";
+import connectDB from "./lib/mongodb.js";
 import userRouter from "./routes/user.route.js";
 import postRouter from "./routes/post.route.js";
 import commentRouter from "./routes/comment.route.js";
+import "dotenv/config";
 const app = express();
 const PORT = process.env.PORT || 4000;
 app.use(express.json());
@@ -10,4 +12,7 @@ app.use("/users", userRouter);
 app.use("/posts", postRouter);
 app.use("/comments", commentRouter);
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+  connectDB();
+});
