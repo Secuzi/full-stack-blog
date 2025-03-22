fbf4610d51ac67ff2b2f75f20c28c499d5b1813f
Secuzi <secre2k@gmail.com>
2025-03-22 12:06:22 +0800

    Initialized comment, user, and post routes

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,13 @@
 import express from "express";
-
+import userRouter from "./routes/user.route.js";
+import postRouter from "./routes/post.route.js";
+import commentRouter from "./routes/comment.route.js";
 const app = express();
 const PORT = process.env.PORT || 4000;
 app.use(express.json());
+
+app.use("/users", userRouter);
+app.use("/posts", postRouter);
+app.use("/comments", commentRouter);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
